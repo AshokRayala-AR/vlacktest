6ab835546e022710d5387db9d8d1b122ced7bd21
NagaRohithvlack <rohith.bhaskarna@vlacksolutions.com>
2024-03-14 04:35:58 +0530

    changes-about us page, features page and send inquiry page responsiveness

--- src/components/aboutus/processfollow/ProcessPage.tsx
+++ src/components/aboutus/processfollow/ProcessPage.tsx
@@ -4,18 +4,18 @@ import SVGcomponent from './processsvg'
 
 function ProcessPage() {
     return (
-        <div className=" flex flex-col gap-10 justify-center items-center p-20">
+        <div className="p-12 flex flex-col gap-10 justify-center items-center md:p-20">
 
             <div className=" ">
                 <h1 className="text-4xl font-bold"> The process we follow </h1>
             </div>
 
-            <div className=" flex space-x-12 p-12">
+            <div className="grid grid-cols-1 md:grid md:grid-cols-2 lg:grid lg:grid-cols-4 lg:space-x-12 md:p-12">
                {Data.map(item => {
                 return (
-                    <div key={item.id} className="">
-                        <SVGcomponent  />
-                        <h1 className="font-semibold font-3xl mt-4 mb-4">{item.title}</h1>
+                    <div key={item.id} className="mb-8 md:mb-0">
+                        <div> <SVGcomponent   /> </div>
+                        <h1 className="font-semibold font-3xl mt-2 mb-2 md:mt-4 md:mb-4">{item.title}</h1>
                         <p className="text-base ">{item.content}</p>
                     </div>
                 )
