8852989b77d6404b76854e33da73df8fef2a0f6c
Bhargva <bhargavagonugunta123@gmail.com>
2024-03-15 10:49:28 +0530

    final chainges

--- src/components/pricing/navbar/navbarcard/NavBarCard.tsx
+++ src/components/pricing/navbar/navbarcard/NavBarCard.tsx
@@ -7,14 +7,14 @@ export default function Navbar() {
     <div>
       <nav className="bg-black text-white text-sm p-0 m-0">
         <ul className="flex justify-end items-center gap-8 py-3 pr-8 ">
-          <li className={`hover:cursor-pointer`}>{t("nav-home")}</li>
+          <li className={`hover:cursor-pointer`} onClick={()=>{navigate("/")}}>{t("nav-home")}</li>
           <li className={`hover:cursor-pointer`} onClick={()=>{navigate("/about")}}>{t("nav-aboutus")}</li>
           <li className={`hover:cursor-pointer`}>{t("nav-features")}</li>
           <li className={`hover:cursor-pointer`} onClick={()=>{navigate("pricing")}}>{t("nav-pricing")}</li>
-          <li className={`hover:cursor-pointer`} >{t("nav-faq")}</li>
-          <li className={`hover:cursor-pointer`} >{t("nav-blog")}</li>
+          <li className={`hover:cursor-pointer`} onClick={()=>{navigate("faq")}} >{t("nav-faq")}</li>
+          <li className={`hover:cursor-pointer`} onClick={()=>{navigate("blogs")}}>{t("nav-blog")}</li>
           <li>
-            <button className="px-8 py-2 border border-white rounded-full">
+            <button className="px-8 py-2 border border-white rounded-full" onClick={()=>{navigate("contact")}}>
               {t("nav-btn-contactus")}
             </button>
           </li>
